e8df4da59275d989e1c7aba71497ac26f5a112ac
Devin Shen <selonsy@gmail.com>
2016-09-03 19:33:53 +0800

    add ufjnls.dll

---
+++ lib/icsharpcode-SharpZipLib-803232d/ICSharpCode.SharpZipLib.Samples/cs/CreateZipFile/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("CreateZipFile")]
+[assembly: AssemblyDescription("Free C# IDE")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
